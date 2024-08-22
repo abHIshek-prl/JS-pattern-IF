fe4ec545ea528205a79e5b705e27dcb332cae679
abhishek <abhishekparayil01@gmail.com>
2024-08-22 09:45:16 +0530

    7-days

---
+++ days/day.js
@@ -0,0 +1,34 @@
+var day=9;
+
+if(day == 1)
+{
+    console.log("Monday")
+}
+else if(day == 2)
+{
+    console.log("tuesday")
+}
+else if(day == 3)
+{
+    console.log("wednesday")
+}
+else if(day == 4)
+{
+    console.log("thursday")
+}
+else if(day == 5)
+{
+    console.log("friday")
+}
+else if(day == 6)
+{
+    console.log("saturday")
+}
+else if(day == 7)
+{
+    console.log("sunday")
+}
+
+else{
+    console.log("NOT VALID")
+}
