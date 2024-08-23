47b86af01574f3714045f9e0df50525a8fb826f4
abhishek <abhishekparayil01@gmail.com>
2024-08-23 22:59:51 +0530

    discout

---
+++ discound/discound.js
@@ -0,0 +1,14 @@
+var amt=100
+
+if(amt>=100)
+{
+    console.log("you have a discount of 20 just pay-" ,amt-20)
+}
+else if(amt>=50 && amt<100)
+{
+    console.log("you have discount of 10 just pay-",amt-10)
+}
+else
+{
+    console.log("you dont have any discount")
+}
