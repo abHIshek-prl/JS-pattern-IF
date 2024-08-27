8b492c8735617f8da662484cc0b586e8d93b3588
abhishek <abhishekparayil01@gmail.com>
2024-08-27 18:25:38 +0530

    mark-grade

---
+++ mark/mark.js
@@ -0,0 +1,30 @@
+var a=20;
+
+if(a>=90)
+{
+    console.log("A+")
+}
+else if(a>=80 && a<90)
+{
+    console.log("A")
+}
+else if(a>=70 && a<80)
+{
+    console.log("B+")
+}
+else if(a>=60 && a<70)
+{
+    console.log("B")
+}
+else if(a>=50 && a<60)
+{
+    console.log("C+")
+}
+else if(a>=40 && a<50)
+{
+    console.log("C")
+}
+else
+{
+    console.log("FAIL")
+}
