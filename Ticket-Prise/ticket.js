0fc5d1b7b156dadfe7ce354d027638adfb592c25
abhishek <abhishekparayil01@gmail.com>
2024-08-29 20:32:44 +0530

    ticket

---
+++ Ticket-Prise/ticket.js
@@ -0,0 +1,18 @@
+var age=25;
+
+if(age<12)
+{
+    console.log("just pay 5")
+}
+else if(age<18 && age>12)
+{
+    console.log("just pay 10")
+}
+else if(age<60 && age>18)
+{
+    console.log("just pay 20")
+}
+else
+{
+    console.log("just jay 15")
+}
