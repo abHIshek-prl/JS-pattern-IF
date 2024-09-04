7a1498351ca1ece8ee2be3fe705da312d37d57ac
abhishek <abhishekparayil01@gmail.com>
2024-09-04 10:27:00 +0530

    pattern

---
+++ pattern2/square.js
@@ -0,0 +1,11 @@
+var i = 1;
+var j = 1;
+//var size = 3;
+for(i = 1; i <= 3; i++) {
+    var row = (i);
+    for(j = 1; j <= 3; j++) {
+        row=row +"abhi ";
+    }
+    
+    console.log(row);
+}
